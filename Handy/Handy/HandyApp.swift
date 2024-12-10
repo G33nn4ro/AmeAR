fde3b609fb3f470f5137d426eea513f05ccebab3
Gennaro Liguori <synchro-behests-01@icloud.com>
2024-12-10 20:21:19 +0100

    Starting a new project

---
+++ Handy/Handy/HandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyApp.swift
+//  Handy
+//
+//  Created by Gennaro Liguori on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
