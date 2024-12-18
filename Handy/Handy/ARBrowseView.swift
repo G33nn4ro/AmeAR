56036ce69aa5748dcec87d351ca1b977e3421adc
Gennaro Liguori <synchro-behests-01@icloud.com>
2024-12-18 12:05:15 +0100

    Structure update

--- Handy/Handy/ARBrowseView.swift
+++ Handy/Handy/ARBrowseView.swift
@@ -20,51 +20,51 @@ struct ARBrowseView: View {
                     
                     
                     // Pulsante rappresentato da un'immagine con un rettangolo arrotondato e testo
-                    NavigationLink(destination: ContentView()) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color.blue)
-                                .frame(width: 370, height: 250)
-                                .shadow(radius: 5)
-                                .overlay(Image("AirPlaneAR")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .foregroundColor(.white))
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                            
-                            VStack {
-                                
-                                VStack {
-                                    Spacer()
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill(
-                                            LinearGradient(
-                                                gradient: Gradient(colors: [Color.black.opacity(0.8), Color.clear]),
-                                                startPoint: .bottom,
-                                                endPoint: .top
-                                            )
-                                        )
-                                        .frame(height: 70)
-                                        .overlay(
-                                            
-                                            HStack {
-                                                Text("Scopri funzione 1")
-                                                    .font(.title2)
-                                                    .fontWeight(.bold)
-                                                    .foregroundColor(.white)
-                                                    .padding(.horizontal)
-                                                
-                                                Spacer()
-                                            }
-                                            
-                                        )
-                                }
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                                .accessibilityLabel(Text("Press the button to execute an action"))
-
-                            }
-                        }
-                    }
+//                    NavigationLink(destination: ContentView()) {
+//                        ZStack {
+//                            RoundedRectangle(cornerRadius: 15)
+//                                .fill(Color.blue)
+//                                .frame(width: 370, height: 250)
+//                                .shadow(radius: 5)
+//                                .overlay(Image("AirPlaneAR")
+//                                    .resizable()
+//                                    .scaledToFill()
+//                                    .foregroundColor(.white))
+//                                .clipShape(RoundedRectangle(cornerRadius: 15))
+//                            
+//                            VStack {
+//                                
+//                                VStack {
+//                                    Spacer()
+//                                    RoundedRectangle(cornerRadius: 15)
+//                                        .fill(
+//                                            LinearGradient(
+//                                                gradient: Gradient(colors: [Color.black.opacity(0.8), Color.clear]),
+//                                                startPoint: .bottom,
+//                                                endPoint: .top
+//                                            )
+//                                        )
+//                                        .frame(height: 70)
+//                                        .overlay(
+//                                            
+//                                            HStack {
+//                                                Text("Scopri funzione 1")
+//                                                    .font(.title2)
+//                                                    .fontWeight(.bold)
+//                                                    .foregroundColor(.white)
+//                                                    .padding(.horizontal)
+//                                                
+//                                                Spacer()
+//                                            }
+//                                            
+//                                        )
+//                                }
+//                                .clipShape(RoundedRectangle(cornerRadius: 15))
+//                                .accessibilityLabel(Text("Press the button to execute an action"))
+//
+//                            }
+//                        }
+//                    }
                     
                     // Pulsante rappresentato da un'immagine con un rettangolo arrotondato e testo
                     NavigationLink(destination: ARTableView()) {
--- Handy/Handy/ARTableView.swift
+++ Handy/Handy/ARTableView.swift
@@ -6,12 +6,72 @@
 //
 
 import SwiftUI
+import RealityKit
+import ARKit
 
 struct ARTableView: View {
+        
+    @State var isPresented: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // 1.
+            Image("toy_car")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.tint)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+                .padding(24)
+
+            // 2.
+            Button {
+                isPresented.toggle()
+                    } label: {
+                        Label("View in AR", systemImage: "arkit")
+                    }.buttonStyle(BorderedProminentButtonStyle())
+                .padding(24)
+
+
+        }
+                .padding()
+                
+        // 3.
+        .fullScreenCover(isPresented: $isPresented, content: {
+           SheetView(isPresented: $isPresented)
+        })
+        
     }
 }
+
+
+
+
+struct SheetView: View {
+    @Binding var isPresented : Bool
+   
+    
+    var body: some View {
+        // 1.
+        ZStack(alignment: .topTrailing) {
+
+            ContentView()
+                .ignoresSafeArea(edges: .all)
+
+            // 2.
+            Button() {
+                isPresented.toggle()
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .background(.ultraThinMaterial)
+                    .clipShape(Circle())
+            }
+            .padding(24)
+        }
+    }
+}
+
 
 #Preview {
     ARTableView()
